1c66e0789e78fb34e56d0981b78364d0c2bd6003
Cleber Rangel <cleberrangeljr@gmail.com>
2022-07-30 21:52:45 -0500

    Painel Cálculos: 183: Implementação do painel de cálculos

--- src/IRPFStonks/IRPFStonks.View/App.xaml.cs
+++ src/IRPFStonks/IRPFStonks.View/App.xaml.cs
@@ -1,11 +1,36 @@
-﻿namespace IRPFStonks.View;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+namespace IRPFStonks.View;
 
 public partial class App : Application
 {
 
+    const int WindowWidth = 800;
+    const int WindowHeight = 800;
+
     public App()
     {
         InitializeComponent();
+
+#if WINDOWS
+
+
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+        });
+
+#endif
 
         MainPage = new AppShell();
     }
